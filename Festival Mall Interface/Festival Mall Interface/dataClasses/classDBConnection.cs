99f320a260d9ba600ce43166eafe36538739936e
Francis Jay Yumul <jfrancis040390@gmail.com>
2016-02-29 10:53:29 +0800

    Commit 02-29-2016
    Initial Files

---
+++ Festival Mall Interface/Festival Mall Interface/dataClasses/classDBConnection.cs
@@ -0,0 +1,185 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+
+namespace Ayala_Interface_dotNet.ClassCon
+{
+    
+     public class classDBConnection 
+     {
+         #region "Declaration
+
+         public OleDbConnection con;
+         public OleDbConnection rmCon;
+         public OleDbConnection tempCon;
+         public OleDbCommand cmd;
+         public OleDbDataReader rdr;
+         public OleDbDataAdapter daTax;
+         public OleDbDataAdapter daDiscount;
+         public OleDbDataAdapter daLessVAT;
+         public OleDbDataAdapter daDiplomat;
+         public OleDbDataAdapter daDeleteRecords;
+         public OleDbDataAdapter daTerminal;
+         public OleDbDataAdapter daGT; 
+         public OleDbCommandBuilder cmdb;
+         
+         public string rmPath { get; set; }
+         public string mdbPath;
+         public string tempPath;
+
+         public DataTable dtGrandTotal { get; set; }
+         #endregion
+
+         #region "Connection Routine"
+
+         //database connection string
+         public classDBConnection()
+         {
+             mdbPath = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|dbLogin.accdb";
+         }
+               
+         //Open MDBConnection
+         public void openConnection()
+         {
+            con = new OleDbConnection(mdbPath);          
+             if (con.State == ConnectionState.Closed) {
+                 con.Open();
+             }
+         }
+
+         public void rmConnect()
+         {
+             string rmConStr;
+             rmConStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + rmPath + ";Extended Properties=dBASE IV;User ID=Admin;Password=;";
+             rmCon = new OleDbConnection(rmConStr);
+             if (rmCon.State == ConnectionState.Closed)
+             {
+                 rmCon.Open();
+             }
+         }
+
+         public void rmDisconnect()
+         {
+             if (rmCon.State == ConnectionState.Open)
+             {
+                 rmCon.Close();
+                 rmCon.Dispose();
+             }
+         }
+
+         public void TemplateConnection(string filePath)
+         {
+             tempPath = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=dBASE IV;User ID=Admin;Password=;";
+             tempCon = new OleDbConnection(tempPath);
+             if (tempCon.State == ConnectionState.Closed)
+             {
+                 tempCon.Open();
+             }
+         }
+
+         public void TemplateConnectionClose()
+         {
+             if (tempCon.State == ConnectionState.Open)
+             {
+                 tempCon.Close();
+             }
+         }
+
+         //close MBDConnection
+         public void closeConnection() 
+         {
+             con = new OleDbConnection(mdbPath);
+             if (con.State == ConnectionState.Open) {
+                 con.Close();
+             }
+         }
+
+         public void closeRMConnect() 
+         {
+             if (rmCon.State == ConnectionState.Open) {
+                 rmCon.Close();
+             }
+         }
+ 
+         #endregion
+
+         #region "Queries Routine"
+        
+         //execute mDB query
+         public void Queries(string sql)
+         {
+             //Open Connection
+             openConnection();
+             //Fire Query
+             cmd = new OleDbCommand(sql, con);
+             rdr = cmd.ExecuteReader();
+             rdr.Read();
+         }
+
+         public void RMQueries(string sql)
+         {
+             //Fire Query
+             cmd = new OleDbCommand(sql, rmCon);
+             rdr = cmd.ExecuteReader();
+             rdr.Read();
+         }
+  
+         //execute datagridview query
+         public void LoadDataGridViewTax(string sql)
+         {
+             daTax = new OleDbDataAdapter();
+             openConnection();
+             daTax = new OleDbDataAdapter(sql, con);
+         }
+
+         public void LoadDataGridViewDiscount(string sql)
+         {
+             daDiscount = new OleDbDataAdapter();
+             openConnection();
+             daDiscount = new OleDbDataAdapter(sql, con);
+         }
+
+         public void LoadDataGridViewLessVAT(string sql)
+         {
+             daLessVAT = new OleDbDataAdapter();
+             openConnection();
+             daLessVAT = new OleDbDataAdapter(sql, con);
+         }
+         
+         public void LoadDataGridViewDiplomat(string sql)
+         {
+             daDiplomat = new OleDbDataAdapter();
+             openConnection();
+             daDiplomat = new OleDbDataAdapter(sql, con);
+         }
+
+         public void LoadDataGridViewTerminal(string sql)
+         {
+             daTerminal = new OleDbDataAdapter();
+             openConnection();
+             daTerminal = new OleDbDataAdapter(sql, con);
+         }
+
+         public void LoadOldGrandTotal(string sql)
+         {
+             daGT = new OleDbDataAdapter();
+             //Open Connection
+             openConnection();
+             //Fire Query
+             daGT = new OleDbDataAdapter(sql, con);
+             dtGrandTotal = new DataTable();
+             daGT.Fill(dtGrandTotal);
+         }
+
+         //execute computation to dbf query
+         public void DBFQuery (string sql)
+         {
+             cmd = new OleDbCommand(sql, tempCon);
+             cmd.ExecuteNonQuery();
+         }
+      
+         #endregion
+              
+     }
+
+}
---
+++ Festival Mall Interface/Festival Mall Interface/dataClasses/classQuery.cs
@@ -0,0 +1,134 @@
+﻿using System.IO;
+using System.Data;
+using System.Data.OleDb;
+namespace Ayala_Interface_dotNet.ClassCon
+{
+    public class classQuery : classDBConnection  
+    {
+
+        #region "Properties"
+
+        public string userPass { get; set; }
+        public string adminPass { get; set; }
+        public string printerPath { get; set; }
+        public string pathRM { get; set; }
+        public string tenantName { get; set; }
+        public string tenantCode { get; set; }
+        public string tenantContract { get; set; }
+        public string ayalaFolder { get; set; }
+        public string currentYear { get; set; }
+        public string ayalaFolderPath { get; set; }
+
+        public DataTable dtTax { get; set; }
+        public DataTable dtDiscount { get; set; }
+        public DataTable dtLessVAT { get; set; }
+        public DataTable dtDiplomat { get; set; }
+        public DataTable dtTerminal { get; set; }
+
+        public bool FolderExist { get; set; }
+        public bool attributeFolder { get; set; }
+
+        #endregion
+
+        #region "Queries Command"
+        
+        //get loginDetailsForLoginForm
+        public void GetLoginDetails()
+        {
+            //SELECT query for login//fire query
+            Queries("Select userPassword,adminPassword FROM tblLogin");
+            //rdr.Read();
+            if (rdr.HasRows) {
+                //read data from database
+                userPass = rdr.GetValue(0).ToString();
+                adminPass = rdr.GetValue(1).ToString();                    
+            }
+        }
+
+        //loadData to Config RMpath,PrinterPath,user and admin pass
+        public void LoadConfigDetails()
+        {
+            Queries("Select * FROM tblLogin");
+            if (rdr.HasRows)
+            {
+                userPass = rdr.GetValue(0).ToString();
+                adminPass = rdr.GetValue(1).ToString();
+                rmPath = rdr.GetValue(2).ToString();
+                printerPath = rdr.GetValue(3).ToString();
+                tenantCode = rdr.GetValue(4).ToString();
+                tenantName = rdr.GetValue(5).ToString();
+                tenantContract = rdr.GetValue(6).ToString();
+                ayalaFolder = rdr.GetValue(7).ToString();
+            }
+        }
+       
+        //loadtaxmap
+        public void LoadTaxMap() 
+        {
+            LoadDataGridViewTax("SELECT TaxTitle as Description, TaxMap as ID,TaxPLU as PLU FROM tblTaxMapping");
+            dtTax = new DataTable();
+            daTax.Fill(dtTax);
+        }
+    
+        //loadRMDiscount
+        public void LoadDiscount() 
+        {
+            LoadDataGridViewDiscount("SELECT DscMap as ID, DscPLU as PLU FROM tblRMDiscount");
+            dtDiscount = new DataTable();
+            daDiscount.Fill(dtDiscount);
+        }
+
+        //loadRMLessVAT
+        public void LoadLessVAT()
+        {
+            LoadDataGridViewLessVAT("SELECT DscMap as ID , DscVAT as PLU FROM tblRMLessVAT");
+            dtLessVAT = new DataTable();
+            daLessVAT.Fill(dtLessVAT);
+        }
+
+        //loadRMDiplomat
+        public void LoadDiplomat()
+        {
+            LoadDataGridViewDiplomat("SELECT DscMap as ID , DscDiplomat as PLU FROM tblRMDiplomat");
+            dtDiplomat = new DataTable();
+            daDiplomat.Fill(dtDiplomat);
+        }
+
+        //loadRMTerminal
+        public void LoadTerminal()
+        {
+            LoadDataGridViewTerminal("SELECT TerminalID as TerminalID FROM tblRMTerminal");
+            dtTerminal = new DataTable();
+            daTerminal.Fill(dtTerminal);
+        }
+
+        #endregion
+
+        #region "Save queries"
+        //save to mdb
+        public void UpdateConfig(string dbParameter, OleDbType dbFieldType, int dbSize, string dbField, string dbValue)
+        {
+            //Open Connection
+            openConnection();
+            //Fire Query
+            cmd = new OleDbCommand("UPDATE tblLogin SET " + dbField + " = " + dbParameter, con);
+            cmd.Parameters.Add(dbParameter, dbFieldType, dbSize, dbField).Value = dbValue;
+            cmd.ExecuteNonQuery();
+        }
+        #endregion
+
+        #region "check if follder exist"
+
+        public void CheckFolderAyala(string currentYear)
+        {
+            //currentYear = DateTime.Today.ToString("yyyy");
+            if (!Directory.Exists(ayalaFolder + "\\" + currentYear))
+            {
+                Directory.CreateDirectory(ayalaFolder + "\\" + currentYear);
+            }          
+        }
+
+        #endregion
+
+    }
+}
